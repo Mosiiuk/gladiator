445be3fd8d575e95e967c593eac208b21cbcbbb3
Gladiator Test Server <noreply@test.gladiatorboost.com>
2026-03-31 16:20:01 +0000

    sync: test server snapshot 2026-03-31T16:20:01Z

--- woocommerce/checkout/thankyou.php
+++ woocommerce/checkout/thankyou.php
@@ -63,14 +63,14 @@ defined( 'ABSPATH' ) || exit;
 			<ul class="woocommerce-order-overview woocommerce-thankyou-order-details order_details">
 
 				<li class="woocommerce-order-overview__order order">
-					<?php esc_html_e( 'Order number:', 'woocommerce' ); ?>
+					<?php esc_html_e( 'Your order number is ', 'woocommerce' ); ?>
 					<strong><?php echo $order->get_order_number(); // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?></strong>
 				</li>
 
-				<li class="woocommerce-order-overview__date date">
+				<!-- <li class="woocommerce-order-overview__date date">
 					<?php esc_html_e( 'Date:', 'woocommerce' ); ?>
 					<strong><?php echo wc_format_datetime( $order->get_date_created() ); // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?></strong>
-				</li>
+				</li> -->
 
 				<?php if ( is_user_logged_in() && $order->get_user_id() === get_current_user_id() && $order->get_billing_email() ) : ?>
 					<li class="woocommerce-order-overview__email email">
@@ -79,10 +79,10 @@ defined( 'ABSPATH' ) || exit;
 					</li>
 				<?php endif; ?>
 
-				<li class="woocommerce-order-overview__total total">
+				<!-- <li class="woocommerce-order-overview__total total">
 					<?php esc_html_e( 'Total:', 'woocommerce' ); ?>
 					<strong><?php echo $order->get_formatted_order_total(); // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?></strong>
-				</li>
+				</li> -->
 
 				<?php if ( $order->get_payment_method_title() ) : ?>
 					<li class="woocommerce-order-overview__payment-method method">
